f39e72c77ab94218971107ba3f00190083bd540c
xy3 <xy3@xycentos.sh.intel.com>
2018-12-15 09:56:00 -0500

    add config file for sample.

--- sample/src/main.cpp
+++ sample/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "utils/power.hpp"
+#include "SampleConfig.h"
 
 using namespace std;
 
@@ -8,6 +9,12 @@ int main() {
 
 	cout << "\n ---> main for cmake\n" << endl;
 
+	//cout << "version major\t" << SAMPLE_VERSION_MAJOR << endl;
+	//cout << "version minor\t" << SAMPLE_VERSION_MINOR << endl;
+	cout << "version major\t" << MY_VERSION_MAJOR << endl;
+	cout << "version minor\t" << MY_VERSION_MINOR << endl;
+
+	cout << endl;
 	cout << "2 power 3 is\t" << power(2, 3) << endl;
 	return 0;
 }
